c6ca70106be1b5db72546e83f0675c38596e6164
tanishqk4 <tanishq_2312res695@iitp.ac.in>
2026-04-10 21:13:22 +0530

    added number of dice rolled

--- Dice Roliing Game/main.py
+++ Dice Roliing Game/main.py
@@ -1,11 +1,18 @@
 import random
 
+count = 0
 while True:
+    
     roll = input("Want to roll the dice? (y/n): ")
     if roll.lower() == "y":
         dice1 = random.randint(1,6)
         dice2 = random.randint(1,6)
         print(f"({dice1}, {dice2})")
-    else:
+        count += 1
+
+    elif roll.lower() == "n":
         print("Thankyou for playing!")
+        print(f"You rolled the dice {count} times.")
         break
+    else:
+        print("Invalid Input")
